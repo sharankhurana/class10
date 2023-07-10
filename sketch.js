635c785990bf5d3e4cae31ab7862d6f4e7a6e9ce
sharankhurana <139131105+sharankhurana@users.noreply.github.com>
2023-07-10 18:55:15 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,3 +1,6 @@
 
 console.log("hello");
-console.log("welcome to the class")
+console.log("hello papa")
+console.log("what are you doing")
+console.log("this is the new thing i learned today")
+console.log("byeeeee")
